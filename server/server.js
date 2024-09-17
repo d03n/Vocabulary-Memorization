fcb708ef5e5ff53c33e546ae4c02813103dd823e
d03n <nattar.1506@gmail.com>
2024-09-17 21:23:22 +0700

    update

--- server/server.js
+++ server/server.js
@@ -29,4 +29,3 @@ mongoose.connect(process.env.MONGO_URI, {
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }).catch(err => console.log(err));
-
