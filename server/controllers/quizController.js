f4b1f24e9b92644fd4830dc4fa1fa468e7deaec4
d03n <nattar.1506@gmail.com>
2024-09-17 21:25:13 +0700

    seperate client and server

--- server/controllers/quizController.js
+++
@@ -1,8 +0,0 @@
-const Vocabulary = require('../models/Vocabulary');
-
-const getQuiz = async (req, res) => {
-    const vocabList = await Vocabulary.find({user: req.user._id});
-    if (!vocabList) return res.status(400).json({message: 'no vocab'});
-
-    
-};
--- server/routes/quizRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getQuiz } = require('../controllers/quizController')
-const authMiddleware = require('../middleware/authMiddleware');
-
-const router = express.Router();
-
-router.get('/', authMiddleware, getQuiz);
-
-module.export = router;
