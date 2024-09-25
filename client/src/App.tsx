a5ed12840034001a98b457e9894e4c1dfb6734a2
d03n <nattar.1506@gmail.com>
2024-09-25 16:48:06 +0700

    save commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+import React from 'react';
+
+const App: React.FC = () {
+  return (
+    <div>
+      <span>Vocabulary Memorization</span>
+    </div>
+  )
+}
+
+export default App;
